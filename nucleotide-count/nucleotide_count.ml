8d66b69f10c12a4d6c634f4bf60538be47fe44f9
Ashish Shrestha <axhixh@gmail.com>
2017-03-21 13:04:38 -0400

    counting iterates over the string only once

--- nucleotide-count/nucleotide_count.ml
+++ nucleotide-count/nucleotide_count.ml
@@ -5,7 +5,16 @@ let count dna n = String.count dna ~f:(fun c -> c = n)
 
 (* Count the nucleotides in the string. *)
 let nucleotide_counts dna =
-  ['A'; 'C'; 'G'; 'T']
-  |> List.map ~f:(fun c -> (c, count dna c))
-  |> List.filter ~f:(fun (_, v) -> v > 0)
-  |> Char.Map.of_alist_exn
+  let inc m c = Char.Map.change m c ~f:(fun v ->
+      match v with
+      | None -> Some 1
+      | Some v -> Some (v + 1)) in
+  String.fold dna ~init:Char.Map.empty ~f:inc
+
+(* iterates the string 4 times
+   let nucleotide_counts dna =
+   ['A'; 'C'; 'G'; 'T']
+   |> List.map ~f:(fun c -> (c, count dna c))
+   |> List.filter ~f:(fun (_, v) -> v > 0)
+   |> Char.Map.of_alist_exn
+*)
