f71d3914bc4235ba36162344d1ea15173e007c1e
Ashish Shrestha <axhixh@gmail.com>
2017-03-20 21:17:54 -0400

    using List from Core

--- difference-of-squares/difference_of_squares.ml
+++ difference-of-squares/difference_of_squares.ml
@@ -1,20 +1,15 @@
-let range n =
-  let rec helper acc i =
-    if i = 0 then 
-      acc
-    else
-      helper (i :: acc) (i - 1) in
-  helper [] n
+open Core.Std
 
 (* Square the sum of the numbers up to the given number *)
 let square_of_sum n = 
-  range n
-  |> List.fold_left (fun acc x -> acc + x) 0 
+  List.range 1 (n + 1)
+  |> List.fold ~init:0 ~f:(fun acc x -> acc + x)  
   |> (fun x -> x * x)
 
 (* Sum the squares of the numbers up to the given number *)
 let sum_of_squares n =
-  List.fold_left (fun acc x -> acc + (x * x)) 0 (range n)
+  List.range 1 (n + 1)
+  |> List.fold_left  ~init:0 ~f:(fun acc x -> acc + (x * x)) 
 
 (* Subtract sum of squares from square of sums *)
 let difference_of_squares n = square_of_sum n - sum_of_squares n
