6b67b806ae31fb2dfd62239e54cf18ac9067a4e1
Ashish Shrestha <axhixh@gmail.com>
2017-03-23 15:56:25 -0400

    using String.exists instead of List.fold; it short circuits

--- bob/bob.ml
+++ bob/bob.ml
@@ -4,9 +4,7 @@ let response_for message =
   let is_question = String.is_suffix ~suffix:"?" in
   let is_shouting s = 
     String.uppercase s = s 
-    && List.fold (String.to_list_rev s) 
-      ~init:false 
-      ~f:(fun a c -> a || Char.is_alpha c) in
+    && String.exists s ~f:Char.is_alpha in
   match String.strip message with
   | "" -> "Fine. Be that way!"
   | m when is_shouting m -> "Whoa, chill out!"
