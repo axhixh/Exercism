85886d4e7f1c90de8cfd5744942020e48bb41ef4
Ashish Shrestha <axhixh@gmail.com>
2017-03-23 16:40:30 -0400

    using list.map instead of fold

--- etl/etl.ml
+++ etl/etl.ml
@@ -3,6 +3,5 @@ open Core.Std
 let transform scrable_points =
   scrable_points
   |> List.fold ~init:[] ~f:(fun acc (point, chars) -> 
-      List.fold chars ~init:acc 
-        ~f:(fun acc' c -> ((Char.lowercase c), point) :: acc'))
-  |> List.sort ~cmp:(fun (l, _) (r, _) -> Char.to_int l - Char.to_int r)
+      List.map chars ~f:(fun c -> ((Char.lowercase c), point)) @ acc)
+      |> List.sort ~cmp:(fun (l, _) (r, _) -> Char.to_int l - Char.to_int r)
