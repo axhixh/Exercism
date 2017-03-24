f5d617016f05df10d09286a8c1484b7b95871e1f
Ashish Shrestha <axhixh@gmail.com>
2017-03-23 21:32:13 -0400

    replacing fold with concat_map

--- etl/etl.ml
+++ etl/etl.ml
@@ -1,7 +1,7 @@
 open Core.Std
 
-let transform scrable_points =
-  scrable_points
-  |> List.fold ~init:[] ~f:(fun acc (point, chars) -> 
-      List.map chars ~f:(fun c -> ((Char.lowercase c), point)) @ acc)
-      |> List.sort ~cmp:(fun (l, _) (r, _) -> Char.to_int l - Char.to_int r)
+let transform scrabble_points =
+  scrabble_points
+  |> List.concat_map  ~f:(fun (point, chars) -> 
+      List.map chars ~f:(fun c -> ((Char.lowercase c), point)))
+  |> List.sort ~cmp:(fun (l, _) (r, _) -> Char.to_int l - Char.to_int r)
