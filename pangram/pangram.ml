15570aa9a0da5e1aab04bbbc0a415d5952d95b04
Ashish Shrestha <axhixh@gmail.com>
2017-03-25 10:32:47 -0400

    use String.for_all instead of fold

--- pangram/pangram.ml
+++ pangram/pangram.ml
@@ -3,4 +3,4 @@ open Core.Std
 let is_pangram s = 
     let s = String.lowercase s in
     "abcdefghijklmnopqrstuvwxyz" 
-    |> String.fold ~init:true ~f:(fun acc c -> acc && String.contains s c)
+    |> String.for_all ~f:(String.contains s)
