b90d5b46ade70b61367cb7f245b65b1b01fc367b
Ashish Shrestha <axhixh@gmail.com>
2017-04-18 20:55:28 -0400

    more idomatic ocaml?

--- run-length-encoding/run_length_encoding.ml
+++ run-length-encoding/run_length_encoding.ml
@@ -8,18 +8,16 @@ let encode s =
   |> String.to_list_rev
   |> List.group ~break:(<>)
   |> List.fold ~init:"" 
-    ~f:(fun acc e -> acc ^ to_string 
-                       (Option.value ~default:' ' (List.hd e))
-                       (List.length e))
+    ~f:(fun acc e -> acc ^ to_string (List.hd_exn e) (List.length e))
 
 let decode s = 
   let generate n c = List.init n ~f:(fun _ -> c) |> String.of_char_list in
   String.fold s ~init:("", None)
-    ~f:(fun (r, counter) c -> match counter with
-        | None when Char.is_digit c 
+    ~f:(fun (r, counter) c -> match (counter, Char.is_digit c) with
+        | (None, true)
           -> (r, Some (Char.to_int c - Char.to_int '0'))
-        | None -> (r ^ generate 1 c, None)
-        | Some n when Char.is_digit c 
+        | (None, false) -> (r ^ generate 1 c, None)
+        | (Some n, true) 
           -> (r, Some (n * 10 + (Char.to_int c - Char.to_int '0')))
-        | Some n -> (r ^ generate n c, None))
+        | (Some n, false) -> (r ^ generate n c, None))
   |> fst
