9c0b0ec9a386dc78d0467135c521c9d6d47ddbde
Ashish Shrestha <axhixh@gmail.com>
2017-05-01 18:35:16 -0400

    using table instead of nested if-else

--- roman-numerals/roman_numerals.ml
+++ roman-numerals/roman_numerals.ml
@@ -1,32 +1,24 @@
+let roman_list = [
+  (1000, "M");
+  (900, "CM");
+  (500, "D");
+  (400, "CD");
+  (100, "C");
+  (90, "XC");
+  (50, "L");
+  (40, "XL");
+  (10, "X");
+  (9, "IX");
+  (5, "V");
+  (4, "IV");
+  (1, "I")]
 
 let to_roman n = 
-  let rec aux acc n =
-    if n >= 1000 then 
-      aux (acc ^ "M") (n - 1000)
-    else if n >= 900 then
-      aux (acc ^ "CM") (n - 900)
-    else if n >= 500 then
-      aux (acc ^ "D") (n - 500)
-    else if n >= 400 then
-      aux (acc ^ "CD") (n - 400)
-    else if n >= 100 then
-      aux (acc ^ "C") (n - 100)
-    else if n >= 90 then
-      aux (acc ^ "XC") (n - 90)
-    else if n >= 50 then
-      aux (acc ^ "L") (n - 50)
-    else if n >= 40 then
-      aux (acc ^ "XL") (n - 40)
-    else if n >= 10 then
-      aux (acc ^ "X") (n - 10)
-    else if n >= 9 then
-      aux (acc ^ "IX") (n - 9)
-    else if n >= 5 then
-      aux (acc ^ "V") (n - 5)
-    else if n >= 4 then
-      aux (acc ^ "IV") (n - 4)
-    else if n > 0 then
-      aux (acc ^ "I") (n - 1)
-    else 
-      acc in
-  aux "" n
+  let rec aux acc n = function
+    |[] -> acc
+    | (v,s)::tl as t -> if n >= v then 
+        aux (acc ^ s) (n - v) t 
+      else
+        aux acc n tl
+  in
+  aux "" n roman_list
