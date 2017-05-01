04a0a2e45504be48de57d0968f2efcb2d3409442
Ashish Shrestha <axhixh@gmail.com>
2017-05-01 19:29:54 -0400

    prime factor - opening Int64 for operators

--- prime-factors/prime_factors.ml
+++ prime-factors/prime_factors.ml
@@ -1,11 +1,10 @@
 open Core.Std
-
-module I = Int64
+open Int64
 
 let factors_of n = 
   let rec aux acc i = function
     | n when n <= 1L -> acc
-    | n when (I.rem n i) = 0L -> aux (i :: acc) i (I.(/) n i)
-    | n -> aux acc (I.succ i) n
+    | n when n % i = 0L -> aux (i :: acc) i (n / i)
+    | n -> aux acc (i + 1L) n
   in
   aux [] 2L n |> List.rev
