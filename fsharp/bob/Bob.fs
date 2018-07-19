267aa068caf56a0c0be05cda744cd7cb4adb1a5b
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2018-07-19 06:27:28 -0400

    fsharp/bob

---
+++ fsharp/bob/Bob.fs
@@ -0,0 +1,12 @@
+﻿module Bob
+
+let response(input : string) = 
+    let trimmed = input.Trim()
+    let empty = trimmed = ""
+    let shout = trimmed = trimmed.ToUpper() && trimmed <> trimmed.ToLower()
+    let question = trimmed.EndsWith "?"
+    if empty then "Fine. Be that way!"
+    elif shout && question then "Calm down, I know what I'm doing!"
+    elif shout then "Whoa, chill out!"
+    elif question then "Sure."
+    else "Whatever."
