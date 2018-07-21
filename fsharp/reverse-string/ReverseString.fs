4a7c8e46eac57f87434eb6cc5f7627a92057a784
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2018-07-21 17:02:17 -0400

    fsharp reverse-string, sum-of-multiples

---
+++ fsharp/reverse-string/ReverseString.fs
@@ -0,0 +1,3 @@
+﻿module ReverseString
+
+let rec reverse (input: string) = input.ToCharArray() |> Array.rev |> (fun c -> System.String c)
---
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,5 @@
+﻿module SumOfMultiples
+
+let sum numbers upperBound = 
+  let isMultiple x = List.exists (fun n -> x % n = 0) numbers
+  [1..(upperBound - 1)] |> List.filter isMultiple |> List.sum
