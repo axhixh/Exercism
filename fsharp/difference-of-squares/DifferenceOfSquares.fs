dced1426c60ef7dcf796a0ee730f73ded689d5cf
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2018-07-23 21:45:37 -0400

    difference of squares - fsharp

---
+++ fsharp/difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,7 @@
+﻿module DifferenceOfSquares
+
+let squareOfSum number = [1..number] |> List.sum |> (fun x -> x * x)
+
+let sumOfSquares number = [1..number] |> List.sumBy (fun x -> x * x)
+
+let differenceOfSquares number = squareOfSum number - sumOfSquares number
