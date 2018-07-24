a79a8c1b0639ecedaf3d843049a62b82f99fdc0c
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2018-07-23 21:46:15 -0400

    collatz-conjecture - fsharp

---
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -0,0 +1,10 @@
+﻿module CollatzConjecture
+
+let steps(number : int) : int option = 
+    let rec helper n step = 
+        match n with
+        | n when n < 1 -> None
+        | 1 -> Some step
+        | n when n % 2 = 0 -> helper (n / 2) (step + 1)
+        | _ -> helper (n * 3 + 1) (step + 1)
+    helper number 0
