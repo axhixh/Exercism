e213b8b6b04790fae568d4f6bd5047f2a799e8f8
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2018-07-23 21:45:53 -0400

    rna transcription - fsharp

---
+++ fsharp/rna-transcription/RnaTranscription.fs
@@ -0,0 +1,12 @@
+﻿module RnaTranscription
+
+let toRna(dna : string) : string = 
+    let mapNucleotide n = 
+        match n with
+        | 'G' -> 'C'
+        | 'C' -> 'G'
+        | 'T' -> 'A'
+        | 'A' -> 'U'
+    dna.ToCharArray()
+    |> Array.map mapNucleotide
+    |> (fun c -> System.String c)
