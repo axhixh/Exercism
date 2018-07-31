890033df49e3420710b09d020306be49b0d75191
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2018-07-31 05:56:28 -0400

    space age in fsharp

---
+++ fsharp/space-age/SpaceAge.fs
@@ -0,0 +1,24 @@
+﻿module SpaceAge
+open System.Numerics
+
+type Planet = 
+  | Earth
+  | Mercury
+  | Venus
+  | Mars
+  | Jupiter
+  | Saturn
+  | Uranus
+  | Neptune
+
+let age (planet: Planet) (seconds: int64): float = 
+  let helper factor = float seconds / 31557600.0 / factor
+  match planet with
+  | Earth -> helper 1.0
+  | Mercury -> helper 0.2408467
+  | Venus -> helper 0.61519726 
+  | Mars -> helper 1.8808158 
+  | Jupiter -> helper 11.862615 
+  | Saturn -> helper 29.447498 
+  | Uranus -> helper 84.016846 
+  | Neptune -> helper 164.79132 
