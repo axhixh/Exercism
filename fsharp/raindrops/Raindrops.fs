d7d86584b5fee52f415f73fb02a14b115dfb11cc
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2018-08-01 22:23:38 -0400

    fsharp - raindrops

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,7 @@
+﻿module Raindrops
+
+let convert (number: int): string =
+  let s = (if number % 3 = 0 then "Pling" else "") + 
+          (if number % 5 = 0 then "Plang" else "") +
+          (if number % 7 = 0 then "Plong" else "")
+  if s = "" then string number else s
