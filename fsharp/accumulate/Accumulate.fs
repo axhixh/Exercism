a317cfa3dcc08c4e7bafd7cbeb2ee0fa10f121ad
Ashish Shrestha <>
2018-08-03 09:56:35 -0400

    fsharp accumulate

---
+++ fsharp/accumulate/Accumulate.fs
@@ -0,0 +1,8 @@
+﻿module Accumulate
+
+let accumulate<'a, 'b> (func: 'a -> 'b) (input: 'a list): 'b list =
+  let rec helper inlist outlist =
+    match inlist with
+    | [] -> outlist
+    | h::rest -> helper rest (outlist @ [(func h)])
+  helper input []
