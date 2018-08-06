ee70143d2a29a39f962c021b5e80332fb0ce5d8b
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2018-08-05 21:59:44 -0400

    fsharp pangram

---
+++ fsharp/pangram/Pangram.fs
@@ -0,0 +1,9 @@
+﻿module Pangram
+
+let isPangram (input: string): bool =
+  let upper = (input.ToUpper ()).ToCharArray ()
+  let contains c =
+    match Array.tryFindIndex (fun u -> c = u) upper with
+    | Some x -> true
+    | None -> false
+  List.forall contains ['A'..'Z']
