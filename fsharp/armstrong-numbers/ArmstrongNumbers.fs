c7ebacf35382ca751fbc799357ce33da5b907d0d
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2018-08-06 22:24:58 -0400

    fsharp armstrong numbers

---
+++ fsharp/armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,7 @@
+﻿module ArmstrongNumbers
+
+let isArmstrongNumber (number: int): bool =
+  let numberOfDigits = floor (log10 (float number) + 1.0)
+  let rec digits n = if n < 10 then [n] else (n % 10) :: digits (n / 10)
+  let sum = digits number |> List.sumBy (fun d -> (float d) ** numberOfDigits)
+  (int sum) = number
