42bcd02a07e4ce0a3c5571f11135528d3ae7d51c
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2018-08-08 07:06:53 -0400

    fsharp kindergarten garden

---
+++ fsharp/kindergarten-garden/KindergartenGarden.fs
@@ -0,0 +1,24 @@
+﻿module KindergartenGarden
+
+type Plant =
+  | Radishes
+  | Clover
+  | Grass
+  | Violets
+
+let children = ["Alice", 0; "Bob", 1; "Charlie", 2; "David", 3;
+    "Eve", 4; "Fred", 5; "Ginny", 6; "Harriet", 7;
+    "Ileana", 8; "Joseph", 9; "Kincaid", 10; "Larry", 11] |> Map.ofList;;
+
+let plants (diagram:string) student =
+  let toPlant c = match c with
+                  | 'R' -> Plant.Radishes
+                  | 'C' -> Plant.Clover
+                  | 'G' -> Plant.Grass
+                  | 'V' -> Plant.Violets
+  diagram.Split '\n'
+  |> Array.map (fun r -> r.ToCharArray () |> Seq.chunkBySize 2 |> Array.ofSeq)
+  |> (fun r -> Array.fold2 (fun a p1 p2 -> a @ [Array.append p1 p2]) [] r.[0] r.[1])
+  |> (fun p -> p.[children.[student]])
+  |> Array.map toPlant
+  |> Array.toList
