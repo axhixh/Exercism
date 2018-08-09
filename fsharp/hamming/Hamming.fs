596eb408c14a5f346c8795fd6aa8271f1f3b1596
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2018-08-08 20:38:00 -0400

    fsharp hamming

---
+++ fsharp/hamming/Hamming.fs
@@ -0,0 +1,10 @@
+﻿module Hamming
+
+let distance (strand1: string) (strand2: string): int option =
+  if strand1.Length <> strand2.Length then
+    None
+  else
+    let s1 = strand1.ToCharArray ()
+    let s2 = strand2.ToCharArray ()
+    let count a n1 n2 = if n1 <> n2 then a + 1 else a
+    Array.fold2 count 0 s1 s2 |> Some
