07f5f7325c2b5260bc231754f16078b2f0e27e9d
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2018-08-12 18:51:48 -0400

    fsharp nucleotide count

---
+++ fsharp/nucleotide-count/NucleotideCount.fs
@@ -0,0 +1,11 @@
+﻿module NucleotideCount
+
+let nucleotideCounts (strand: string): Map<char, int> option =
+  let empty = [('A',0); ('C',0); ('G', 0); ('T',0)] |> Map.ofList |> Some
+  let count c (m:Map<char, int>) = match m.TryFind(c) with
+                                   | Some v -> Some (m.Add(c, (v + 1)))
+                                   | None -> None
+  strand.ToCharArray () 
+  |> Array.fold (fun a c -> match a with
+                            | Some m -> count c m
+                            | None -> None) empty
