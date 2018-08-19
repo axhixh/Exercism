8b071b9ebc8c8e12671ed2e15afbb204a9115956
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2018-08-19 11:08:59 -0400

    fsharp allergies

---
+++ fsharp/allergies/Allergies.fs
@@ -0,0 +1,14 @@
+﻿module Allergies
+
+open System
+
+type Allergen = Eggs = 1 | Peanuts = 2 | Shellfish = 4 | Strawberries = 8 
+                | Tomatoes = 16 | Chocolate = 32 | Pollen = 64 | Cats = 128
+
+let allergicTo codedAllergies (allergen:Allergen) = codedAllergies &&& (int allergen) > 0
+
+let list codedAllergies = 
+    System.Enum.GetValues (typeof<Allergen>) 
+    |> Seq.cast<Allergen>
+    |> Seq.filter (allergicTo codedAllergies)
+    |> Seq.toList
